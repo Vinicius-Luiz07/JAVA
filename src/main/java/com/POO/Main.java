c453f6723927d12399c7d142fd810201c5d50226
Vinicius-Luiz07 <viniciusluiz8058@gmail.com>
2026-05-21 21:01:22 -0300

    atualizando a main

--- src/main/java/com/POO/Main.java
+++ src/main/java/com/POO/Main.java
@@ -1,12 +1,17 @@
 package com.POO;
 
 import com.POO.Calculadora_POO.Calculadora;
+import com.POO.Converter_Temperatura_POO.ConverterTemperatura;
 
 public class Main {
     public static void main(String[] args) {
         
         Calculadora calculadora = new Calculadora(); 
-
         System.out.println(calculadora);
+
+        /* .:|====== && =====|:.*/
+
+        ConverterTemperatura conversor = new ConverterTemperatura();
+        System.out.println(conversor);
     }
 }
