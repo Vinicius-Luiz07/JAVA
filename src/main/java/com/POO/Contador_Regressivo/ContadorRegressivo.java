28e8ec009fa9947e60e8821a49f8118e8abbf426
! Morango 苺 <119367415+Vinicius-Luiz07@users.noreply.github.com>
2026-05-28 22:33:22 -0300

    fix: consertando o problema de não apagar as mensagens antigas do console

--- src/main/java/com/POO/Contador_Regressivo/ContadorRegressivo.java
+++ src/main/java/com/POO/Contador_Regressivo/ContadorRegressivo.java
@@ -3,6 +3,7 @@ package com.POO.Contador_Regressivo;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
 
 public class ContadorRegressivo  {
     // Método principal
@@ -14,6 +15,8 @@ public class ContadorRegressivo  {
         System.out.println("Digite um valor: ");
         int valor = leia.nextInt();
 
+        CountDownLatch latch = new CountDownLatch(1); // trava a thread principal(switch case) é ESSENCIAL ter.
+
         timer.schedule(new TimerTask() {   // ← Agendando a tarefa
 
             int contador = valor;
@@ -22,12 +25,40 @@ public class ContadorRegressivo  {
             public void run() {
                 System.out.println("Contando: " + contador);
                 contador--;
-                
-            if (contador < 0) {
-                System.out.println("Timer finalizado!");
-                timer.cancel();   // Parar o timer
+                if (contador < 0) {
+                    System.out.println("Temporizador finalizado!");
+                    timer.cancel();
+
+                    try { // try essencial para apagar as mensagens antigas do console
+                        System.out.println("Apagando logs...");
+                        Thread.sleep(3000); // aguarda 3 segundos para apagar o prompt
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    clearConsole(valor); // limpa o console
+                    latch.countDown(); // libera a thread principal (swith case)
+                }
             }
+        }, 0, 1000); // para a cada 1000 = 1 segundo
+
+        try {
+            latch.await(); // -> trave aqui até o countDown() ser chamado
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }, 0, 1000);
+    }
+
+    private void clearConsole(int linhas) {
+        // método utilizado para apagar o console
+           try {
+                if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
